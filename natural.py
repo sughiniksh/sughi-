a7525d9813171e47d6d062a496bb158ad57799ab
sughiniksh <42801281+sughiniksh@users.noreply.github.com>
2018-09-24 13:50:52 +0530

    Update natural.py

--- natural.py
+++ natural.py
@@ -1,6 +1,6 @@
-n=int(input("Enter a num: "))
-s1 = 0
-while(n > 0):
-    s1=s1+n
-    n=n-1
-print("Sum of first n natural numbers is",s1)
+l=int(input("Enter a num: "))
+x = 0
+while(l > 0):
+    x=x+l
+    l=l-1
+print("Sum of first n natural numbers is",x)
