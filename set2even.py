45ff68863b882d0982a2b3ef3f5b676b69b74768
sughiniksh <42801281+sughiniksh@users.noreply.github.com>
2018-09-26 10:45:09 +0530

    Update set2even.py

--- set2even.py
+++ set2even.py
@@ -1,5 +1,4 @@
-a= int(input(""))
-b= int(input(""))
-for i in range(a,b+1):
-  if(i%2 == 0):
-    print(i)
+a,b=map(int,input().split(' '))
+for i in range(a+1,b):
+    if (i % 2 == 0):
+        print(i)
