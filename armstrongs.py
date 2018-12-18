47877fc067652489d2bc9374e7fb18bf15ea1241
sughiniksh <42801281+sughiniksh@users.noreply.github.com>
2018-12-18 12:24:53 +0530

    Create armstrongs.py

---
+++ armstrongs.py
@@ -0,0 +1,14 @@
+num = int(input(""))  
+sum = 0  
+temp = num  
+  
+while temp > 0:  
+   digit = temp % 10  
+   sum += digit ** 3  
+   temp //= 10  
+  
+if num == sum:  
+   print(num,"yes")  
+else:  
+   print(num,"no")  
+   
