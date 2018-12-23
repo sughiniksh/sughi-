99daa01a88d0db2eae481767145820b3aaf6a09a
sughiniksh <42801281+sughiniksh@users.noreply.github.com>
2018-12-23 18:19:45 +0530

    Update powerof2.py

--- powerof2.py
+++ powerof2.py
@@ -1,12 +1,10 @@
-def isPowerOfTwo(n): 
-    if (n == 0): 
+def is_power_of_two(s):
+    if s <= 0:
         return False
-    while (n != 1): 
-            if (n % 2 != 0): 
-                return False
-            n = n // 2
-    return True
-if(isPowerOfTwo(64)): 
-    print('Yes') 
-else: 
-    print('No') 
+    else:
+        return s & (s - 1) == 0
+s = int(input(''))
+if is_power_of_two(s):
+    print('yes'.format(s))
+else:
+    print('no'.format(s))
